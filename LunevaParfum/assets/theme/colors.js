f0d139a58185829dab0620f5e3f30db8574a36a9
Alvyn Ardhana <alvynardhana29@gmail.com>
2026-05-02 22:10:24 +0700

    Tugas 2 - Luneva Parfum

---
+++ LunevaParfum/assets/theme/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  grey: (opacity = 1) => `rgba(120,120,120,${opacity})`,
+  blue: (opacity = 1) => `rgba(80,120,255,${opacity})`,
+  white: () => '#FFFFFF',
+  black: () => '#000000',
+};
+
+export default colors;
